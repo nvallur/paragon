d0637464a85b3101d95c3d470231937c3c51d1e4
Nandini Valluru <130528997+nvallur@users.noreply.github.com>
2023-12-03 19:57:57 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,6 +22,86 @@ if(window.location.href.indexOf('login.html') > -1){
 });
 }
 console.log(localStorage.getItem("username"));
+if(window.location.href.indexOf('subject_a_bio_stats.html') > -1){
+    const statsDivs = document.getElementsByClassName("stat");
+    const hidden = document.getElementsByClassName("hidden");
+    const info = document.getElementsByClassName("stat_info");
+
+    for (let i = 0; i < statsDivs.length; i++){
+        statsDivs[i].addEventListener("mouseover", function(){
+            for (let j = 0; j < hidden.length; j++){
+               
+                hidden[j].style.display = "block";
+            }
+            for (let k = 0; k < info.length; k++){
+                info[k].style.display = "none";
+            }
+            
+            
+
+            
+        });
+        statsDivs[i].addEventListener("mouseout", function(){
+            for (let j = 0; j < hidden.length; j++){
+                hidden[j].style.display = "none";
+            }
+            for (let k = 0; k < info.length; k++){
+                info[k].style.display = "block";
+            }
+        });
+       
+    }
+    for (let i = 0; i < statsDivs.length; i++){
+        statsDivs[i].addEventListener("focus", function(){
+            for (let j = 0; j < hidden.length; j++){
+               
+                hidden[j].style.display = "block";
+            }
+            for (let k = 0; k < info.length; k++){
+                info[k].style.display = "none";
+            }
+            
+            
+
+            
+        });
+        statsDivs[i].addEventListener("blur", function(){
+            for (let j = 0; j < hidden.length; j++){
+                hidden[j].style.display = "none";
+            }
+            for (let k = 0; k < info.length; k++){
+                info[k].style.display = "block";
+            }
+        });
+       
+    }
+    for (let i = 0; i < statsDivs.length; i++){
+        statsDivs[i].addEventListener("click", function(){
+            for (let j = 0; j < hidden.length; j++){
+               
+                hidden[j].style.display = "block";
+            }
+            for (let k = 0; k < info.length; k++){
+                info[k].style.display = "none";
+            }
+            
+            
+
+            
+        });
+        statsDivs[i].addEventListener("blur", function(){
+            for (let j = 0; j < hidden.length; j++){
+                hidden[j].style.display = "none";
+            }
+            for (let k = 0; k < info.length; k++){
+                info[k].style.display = "block";
+            }
+        });
+       
+    }
+    
+   
+}
 
 
 
@@ -34,6 +114,8 @@ if (window.location.href.indexOf('terminate.html') > -1) {
    console.log("terminate");
    // Add a specified delay in miliseconds
 const wait = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms))
+const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+console.log(mediaQuery['matches']);
 
 // Write text to a target element with a specified delay in ms
 function writeText(target, content, delay = 100)
@@ -62,10 +144,22 @@ function writeText(target, content, delay = 100)
     }
   })
 }
-  
+function writeNoAnimation(target, content) {
+  target.innerHTML = content
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
+
     await wait(5000)
+    if (mediaQuery['matches']) {
+        await writeNoAnimation(document.getElementById('name'), `Hello, ${localStorage.getItem("username")}.`)
+        await writeNoAnimation(document.getElementById('instructions'), `If you're reading this. It's not too late. You can still run.`)
+        await wait(2000)
+        await writeNoAnimation(document.getElementById('creepy'), `\nBut you can't hide.`)
+        await wait(1000)
+        await writeNoAnimation(document.getElementById('creepy2'), `\nRemember, they're always watching.`)
+        return
+    }
     const instructions = document.getElementById('instructions')
     const creepy = document.getElementById('creepy')
     const creepy2 = document.getElementById('creepy2')
